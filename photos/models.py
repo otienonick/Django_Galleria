f6d8292b2f9967f3c308e4909cf9faf53bbd792b
otienonick <nick.odongi@student.moringaschool.com>
2021-10-08 01:17:51 +0300

    added models to the admin page and also  created a superuser

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    location = models.ForeignKey(Location,on_delete = models.SET_NULL,null=True)
+    category = models.ForeignKey(Category,on_delete = models.SET_NULL,null=True)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'articles/',null = False)
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.description
