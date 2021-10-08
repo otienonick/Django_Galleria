b97d5988c7d1d24b38fac7e68b405c8fffb118cb
otienonick <nick.odongi@student.moringaschool.com>
2021-10-08 13:52:41 +0300

    connected search url

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('' , views.gallery, name = 'gallery'),
     path('photo/<str:pk>/' , views.viewPhoto, name='photo'),
+    path('search/', views.search_results, name ='search_results'),
+
 
 ]
 
