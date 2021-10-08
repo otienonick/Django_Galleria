5824fc830308f9df817f6f19f324717332caaa20
otienonick <nick.odongi@student.moringaschool.com>
2021-10-08 08:40:20 +0300

    modified gallery view function to get photos,locations and categories

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Category,Photo,Location
 
 # Create your views here.
+
 def gallery(request):
-    return render(request,'photos/gallery.html')
+    # category = request.GET.get('category')
+    location = request.GET.get('location')
+
+    if location ==  None:
+        photos = Photo.objects.all()
+    else:
+        # photos = Photo.objects.filter(category__name = category)
+        photos = Photo.objects.filter(location__name = location)
+
+
+
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+
+    return render(request,'photos/gallery.html', {"categories" : categories,"photos" : photos,"locations" : locations})
+
