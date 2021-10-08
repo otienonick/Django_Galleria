9bb47dad49e4fee15bfba1d83666d7546e58c027
otienonick <nick.odongi@student.moringaschool.com>
2021-10-08 09:10:28 +0300

    added viewPhoto view function to display a particular image id

--- photos/views.py
+++ photos/views.py
@@ -4,13 +4,11 @@ from .models import Category,Photo,Location
 # Create your views here.
 
 def gallery(request):
-    # category = request.GET.get('category')
     location = request.GET.get('location')
 
     if location ==  None:
         photos = Photo.objects.all()
     else:
-        # photos = Photo.objects.filter(category__name = category)
         photos = Photo.objects.filter(location__name = location)
 
 
@@ -20,3 +18,8 @@ def gallery(request):
 
     return render(request,'photos/gallery.html', {"categories" : categories,"photos" : photos,"locations" : locations})
 
+def viewPhoto(request,pk):
+    photo = Photo.objects.get(id = pk)
+
+
+    return render(request,'photos/photo.html', {"photo" : photo})
