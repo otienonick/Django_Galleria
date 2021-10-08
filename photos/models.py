8060d3d6efe0e27556b0214742892621583706bb
otienonick <nick.odongi@student.moringaschool.com>
2021-10-08 19:04:36 +0300

    Ran test and all passed successfully

--- photos/models.py
+++ photos/models.py
@@ -26,6 +26,33 @@ class Photo(models.Model):
     def __str__(self):
         return self.description
 
+    def save_image(self):
+        self.save() 
+
+    def delete_image(self):
+        self.delete()  
+
+    def  update_image(self):
+        self.save()  
+
+    @classmethod
+    def filter_by_location(cls):
+        locate = Location
+        image = cls.objects.filter(location__name = locate)
+        return image
+
+    @classmethod
+    def search_image(cls):
+        search_term = Category
+        photos = cls.objects.filter(category__name = search_term)   
+        return photos   
+
+    @classmethod
+    def get_image_by_id(cls,id=None):
+        photos = cls.objects.filter(id = id)   
+        return photos         
+   
+           
     @classmethod
     def search_by_category(cls,search_term):
         photos = cls.objects.filter(category__name__icontains = search_term)   
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,74 @@
 from django.test import TestCase
-
+from .models import Photo,Category,Location
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.new_location = Location(name = 'test_name')
+        self.new_location.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_location,Location))     
+
+class CategoryTestClass(TestCase):
+
+    def setUp(self):
+        self.new_category = Category(name = 'test_name')
+        self.new_category.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_category,Category))   
+
+class PhotoTestClass(TestCase):
+        def setUp(self):
+
+            self.new_location = Location(name = 'test_name')
+            self.new_location.save()
+
+            self.new_category = Category(name = 'test_name')
+            self.new_category.save()
+
+            self.new_image = Photo(location = self.new_location,category = self.new_category,name ='test_name',description = 'test_description' )
+
+        def test_instance(self):
+            self.assertTrue(isinstance(self.new_image,Photo))   
+
+        def test_save_method(self):
+            self.new_image.save_image()
+            images = Photo.objects.all()
+            self.assertTrue(len(images) > 0)    
+
+        def tearDown(self):
+            Photo.objects.all().delete()
+            Category.objects.all().delete()
+            Location.objects.all().delete()
+
+        def test_delete_method(self):
+            self.new_image.save_image()
+            self.new_image.delete_image()
+            images = Photo.objects.all()
+            self.assertTrue(len(images) == 0)    
+
+        def test_update_method(self):
+            self.new_image.save_image()
+            self.new_image.update_image()
+            images = Photo.objects.all()
+            self.assertTrue(len(images) > 0) 
+
+        def test_get_image_by_id(self):
+            id = Photo.get_image_by_id()
+            self.assertTrue(len(id) == 0)    
+
+
+        def test_filter_by_location(self):
+            location = Photo.filter_by_location()
+            self.assertTrue(len(location) == 0)
+
+        def test_search_image(self):
+            category = Photo.search_image()
+            self.assertTrue(len(category)  == 0)
+                
+            
+                    
+
